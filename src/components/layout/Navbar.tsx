93f5b67339458e1ca3e07b9ede20a086696d5a0d
Tanaca <jtanacar@gmail.com>
2022-07-21 16:06:22 -0300

    chore: add 'em breve' in navbar menus

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,22 +1,31 @@
+import Badge from '@atoms/Badge';
 import React from 'react';
 
 const Navbar = () => {
   return (
     <div className="relative z-20">
       <div className="absolute w-full py-7 flex items-center justify-between px-[5.5rem]">
-        <span className="text-2xl font-semibold">Tanaca</span>
-        <ul className="flex gap-12 text-base font-medium text-gray-500">
-          <li className="text-gray-900">
-            <a>Pricing</a>
+        <span className="text-3xl font-bold">Tanaca</span>
+        <ul className="flex relative gap-12 text-base font-medium text-gray-500">
+          <div className="absolute -top-full -left-7 w-full flex gap-12">
+            <Badge className="whitespace-nowrap -top-full -left-12 text-xs">
+              Em breve
+            </Badge>
+            <Badge className="whitespace-nowrap -top-full -left-12 text-xs">
+              Em breve
+            </Badge>
+            <Badge className="whitespace-nowrap -top-full -left-12 text-xs">
+              Em breve
+            </Badge>{' '}
+          </div>
+          <li>
+            <a>Sobre</a>
           </li>
           <li>
-            <a>About</a>
+            <a>Contato</a>
           </li>
           <li>
-            <a>Post</a>
-          </li>
-          <li>
-            <a>Contact</a>
+            <a>Blog</a>
           </li>
         </ul>
       </div>

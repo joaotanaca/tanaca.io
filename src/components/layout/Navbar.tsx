bad44b7053da9700143352d86318da8970bf9127
Tanaca <jtanacar@gmail.com>
2022-08-02 20:55:23 -0300

    chore: change "Tanaca" to logo icon

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,23 +1,13 @@
 import Badge from '@atoms/Badge';
+import Icon from '@atoms/Icon';
 import React from 'react';
 
 const Navbar = () => {
   return (
     <div className="relative z-20">
       <div className="absolute w-full py-12 md:py-7 flex items-center justify-center md:justify-between md:px-[5.5rem]">
-        <span className="hidden md:block text-3xl font-bold">Tanaca</span>
+        <Icon icon="logoWithText" size={260} />
         <ul className="flex relative gap-12 text-base font-medium text-gray-500">
-          <div className="absolute -top-full -left-7 w-full flex gap-12">
-            <Badge className="whitespace-nowrap -top-full -left-12 text-xs">
-              Em breve
-            </Badge>
-            <Badge className="whitespace-nowrap -top-full -left-12 text-xs">
-              Em breve
-            </Badge>
-            <Badge className="whitespace-nowrap -top-full -left-12 text-xs">
-              Em breve
-            </Badge>{' '}
-          </div>
           <li>
             <a>Sobre</a>
           </li>
